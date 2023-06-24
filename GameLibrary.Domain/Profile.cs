6ad723f3aae9bb5bfc07bef6ed2b2e4018912ce4
Kamil Cieślik <cieslik.kamil@outlook.com>
2023-06-24 21:54:55 +0200

    start of basic configuration for database

---
+++ GameLibrary.Domain/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace GameLibrary.Domain;
+
+public class Profile
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Salt { get; set; }
+    public string Email { get; set; }
+
+}
